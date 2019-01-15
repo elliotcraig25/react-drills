6d8a476346a14142281169f8a9a88db3a71e87dc
elliot <elliotgcraig@icloud.com>
2019-01-15 09:28:38 -0700

    working on problem 7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewTask from './Components/NewTask'
+import ToDo from './Components/ToDo'
+import List from './Components/List'
 
 class App extends Component {
   render() {
+    let variable = <ToDo />
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do List:</h1>
+        <NewTask />
+        <List />
       </div>
     );
   }
---
+++ app-7/src/Components/ToDo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ToDo extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ToDo
